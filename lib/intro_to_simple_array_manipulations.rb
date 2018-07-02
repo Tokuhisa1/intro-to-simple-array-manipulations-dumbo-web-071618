edb0b102c27738100b3ce5031723ceb2c4336f85
Winston Tokuhisa <tokuhisawinston@outlook.com>
2018-07-02 18:00:51 +0000

    automatically backed up by learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -26,3 +26,7 @@ def using_concat(array1, array2)
   # https://ruby-doc.org/core-2.2.0/Array.html#method-i-concat
   array1.concat(array2)
 end
+
+def using_insert(array, element)
+  array.insert(string)
+end
